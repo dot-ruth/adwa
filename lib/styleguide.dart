9a1efe87e47c627b8dced396b2daec9989663d26
dot-ruth <ruthgetaneh5@gmail.com>
2025-03-02 09:25:46 +0300

    fix: updated font size

--- lib/styleguide.dart
+++ lib/styleguide.dart
@@ -5,7 +5,7 @@ class AppTheme {
   static const TextStyle display1 = TextStyle(
     fontFamily: 'WorkSans',
     color: Colors.black,
-    fontSize: 38,
+    fontSize: 32,
     fontWeight: FontWeight.w600,
     letterSpacing: 1.2,
   );
@@ -13,7 +13,7 @@ class AppTheme {
   static const TextStyle display2 = TextStyle(
     fontFamily: 'WorkSans',
     color: Colors.black,
-    fontSize: 32,
+    fontSize: 26,
     fontWeight: FontWeight.normal,
     letterSpacing: 1.1,
   );
@@ -21,7 +21,7 @@ class AppTheme {
   static final TextStyle heading = TextStyle(
     fontFamily: 'WorkSans',
     fontWeight: FontWeight.w900,
-    fontSize: 34,
+    fontSize: 24,
     color: Colors.black,
     letterSpacing: 1.2,
   );
@@ -30,7 +30,7 @@ class AppTheme {
     inherit: true,
     fontFamily: 'WorkSans',
     fontWeight: FontWeight.w500,
-    fontSize: 24,
+    fontSize: 14,
     color: Colors.black,
   );
 }
